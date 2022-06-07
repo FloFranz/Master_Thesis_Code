471dd4daf871bbe041cad17930ff8c96fcac7890
FloFranz <florian.franz86@gmx.de>
2022-06-07 12:52:19 +0200

    Update tree_heights.R

--- scripts/tree_heights.R
+++ scripts/tree_heights.R
@@ -34,9 +34,15 @@ ndsm_reinhardshagen_tile3 <- terra::rast(paste0(filepath_reinhardshagen_ndsm_air
                                                 "ndsm_325415699.tif"))
 
 ndsm_neukirchen8_tile1 <- terra::rast(paste0(filepath_neukirchen8_ndsm_aircraft,
-                                             "ndsm_325255645.tif"))
+                                             "ndsm_325255644.tif"))
 
 ndsm_neukirchen8_tile2 <- terra::rast(paste0(filepath_neukirchen8_ndsm_aircraft,
+                                             "ndsm_325255645.tif"))
+
+ndsm_neukirchen8_tile3 <- terra::rast(paste0(filepath_neukirchen8_ndsm_aircraft,
+                                             "ndsm_325275644.tif"))
+
+ndsm_neukirchen8_tile4 <- terra::rast(paste0(filepath_neukirchen8_ndsm_aircraft,
                                              "ndsm_325275645.tif"))
 
 ndsm_neukirchen9_tile1 <- terra::rast(paste0(filepath_neukirchen9_ndsm_aircraft,
@@ -46,75 +52,94 @@ ndsm_neukirchen9_tile2 <- terra::rast(paste0(filepath_neukirchen9_ndsm_aircraft,
                                              "ndsm_325235639.tif"))
 
 # Drone --> currently still DSM, has to be changed to nDSM!!! #
-dsm_drone_reinhardshagen1 <- terra::rast("J:/data/drone/reinhardshagen_e_str1_bis_4/DSM/Reinhardshagen_550_A_2_DEM10.tif")
-dsm_drone_reinhardshagen2 <- terra::rast("J:/data/drone/reinhardshagen_e_str1_bis_4/DSM/Reinhardshagen_560_A_4_DEM10.tif")
+dsm_drone_reinhardshagen_1 <- terra::rast("J:/data/drone/reinhardshagen_e_str1_bis_4/DSM/Reinhardshagen_550_A_2_DEM10.tif")
+dsm_drone_reinhardshagen_2 <- terra::rast("J:/data/drone/reinhardshagen_e_str1_bis_4/DSM/Reinhardshagen_560_A_4_DEM10.tif")
 
-dsm_drone_neukirchen91 <- terra::rast("J:/data/drone/neukirchen_e_str9/DSM/Neukirchen_521_C_1_DEM10.tif")
-dsm_drone_neukirchen92 <- terra::rast("J:/data/drone/neukirchen_e_str9/DSM/Neukirchen_525_B_1_DEM10.tif")
+dsm_drone_neukirchen8_1 <- terra::rast("J:/data/drone/neukirchen_e_str8/DSM/Neukirchen_1071_B_1_DEM10.tif")
+dsm_drone_neukirchen8_2 <- terra::rast("J:/data/drone/neukirchen_e_str8/DSM/Neukirchen_1213_C_1_DEM10.tif")
+
+dsm_drone_neukirchen9_1 <- terra::rast("J:/data/drone/neukirchen_e_str9/DSM/Neukirchen_521_C_1_DEM10.tif")
+dsm_drone_neukirchen9_2 <- terra::rast("J:/data/drone/neukirchen_e_str9/DSM/Neukirchen_525_B_1_DEM10.tif")
 
 
 # Data preprocessing
 #--------------------
 # Aircraft #
 
-# Merge raster files (nDSMs) at one location into one
-ndsm_aircraft_reinhardshagen <- terra::merge(ndsm_reinhardshagen_tile1,
-                                             ndsm_reinhardshagen_tile2,
-                                             ndsm_reinhardshagen_tile3)
+# Merge tiles (nDSMs) at one location into one
+ndsm_aircraft_reinhardshagen_merged <- terra::merge(ndsm_reinhardshagen_tile1,
+                                                    ndsm_reinhardshagen_tile2,
+                                                    ndsm_reinhardshagen_tile3)
 
-ndsm_aircraft_neukirchen9 <- terra::merge(ndsm_neukirchen9_tile1,
-                                          ndsm_neukirchen9_tile2)
+ndsm_aircraft_neukirchen8_1_merged <- terra::merge(ndsm_neukirchen8_tile1,
+                                                   ndsm_neukirchen8_tile2)
 
-# Crop out subsets of the aircraft nDSMs
+ndsm_aircraft_neukirchen8_2_merged <- terra::merge(ndsm_neukirchen8_tile3,
+                                                   ndsm_neukirchen8_tile4)
+
+ndsm_aircraft_neukirchen9_merged <- terra::merge(ndsm_neukirchen9_tile1,
+                                                 ndsm_neukirchen9_tile2)
+
+# Crop out subsets of the merged aircraft nDSMs
 # corresponding to the extent of the respective drone nDSMs
 
 # Compare CRS
-raster::compareCRS(ndsm_aircraft_reinhardshagen, dsm_drone_reinhardshagen1)
-raster::compareCRS(ndsm_aircraft_reinhardshagen, dsm_drone_reinhardshagen2)
+raster::compareCRS(ndsm_aircraft_reinhardshagen_merged, dsm_drone_reinhardshagen_1)
+raster::compareCRS(ndsm_aircraft_reinhardshagen_merged, dsm_drone_reinhardshagen_2)
 
-raster::compareCRS(ndsm_aircraft_neukirchen9, dsm_drone_neukirchen91)
-raster::compareCRS(ndsm_aircraft_neukirchen9, dsm_drone_neukirchen92)
+raster::compareCRS(ndsm_aircraft_neukirchen8_1_merged, dsm_drone_neukirchen8_1)
+raster::compareCRS(ndsm_aircraft_neukirchen8_2_merged, dsm_drone_neukirchen8_2)
+
+raster::compareCRS(ndsm_aircraft_neukirchen9_merged, dsm_drone_neukirchen9_1)
+raster::compareCRS(ndsm_aircraft_neukirchen9_merged, dsm_drone_neukirchen9_2)
 
 # Get extent of drone nDSMs
-terra::ext(dsm_drone_reinhardshagen1)
-terra::ext(dsm_drone_reinhardshagen2)
+terra::ext(dsm_drone_reinhardshagen_1)
+terra::ext(dsm_drone_reinhardshagen_2)
 
-terra::ext(dsm_drone_neukirchen91)
-terra::ext(dsm_drone_neukirchen92)
+terra::ext(dsm_drone_neukirchen8_1)
+terra::ext(dsm_drone_neukirchen8_2)
+
+terra::ext(dsm_drone_neukirchen9_1)
+terra::ext(dsm_drone_neukirchen9_2)
 
 # Define this extent new (UTM zone-number 32 has to be removed)
-terra::ext(dsm_drone_reinhardshagen1) <- terra::ext(541261.1117562, 542399.480701,
-                                                    5698193.6553981, 5698869.39952059)
+terra::ext(dsm_drone_reinhardshagen_1) <- terra::ext(541261.1117562, 542399.480701,
+                                                     5698193.6553981, 5698869.39952059)
 
-terra::ext(dsm_drone_reinhardshagen2) <- terra::ext(541258.7071286, 541461.8301163,
-                                                    5698996.4575736, 5699284.04853613)
+terra::ext(dsm_drone_reinhardshagen_2) <- terra::ext(541258.7071286, 541461.8301163,
+                                                     5698996.4575736, 5699284.04853613)
 
-terra::ext(dsm_drone_neukirchen91) <- terra::ext(522260.5142122, 522545.7018505,
-                                                 5639230.99173351, 5639473.19630677)
+terra::ext(dsm_drone_neukirchen8_1) <- terra::ext(527660.8964696, 527827.3314433,
+                                                  5644953.32855214, 5645106.86856389)
 
-terra::ext(dsm_drone_neukirchen92) <- terra::ext(522746.2034559, 523065.677675,
-                                                 5639684.5060757, 5639935.40729279)
+terra::ext(dsm_drone_neukirchen8_2) <- terra::ext(525125.9142932, 525547.7488196,
+                                                  5644903.61658953, 5645566.05673327)
+
+terra::ext(dsm_drone_neukirchen9_1) <- terra::ext(522260.5142122, 522545.7018505,
+                                                  5639230.99173351, 5639473.19630677)
+
+terra::ext(dsm_drone_neukirchen9_2) <- terra::ext(522746.2034559, 523065.677675,
+                                                  5639684.5060757, 5639935.40729279)
 
 # Crop out subsets
-ndsm_aircraft_reinhardshagen1 <- terra::crop(ndsm_aircraft_reinhardshagen,
-                                             dsm_drone_reinhardshagen1)
+ndsm_aircraft_reinhardshagen_1 <- terra::crop(ndsm_aircraft_reinhardshagen_merged,
+                                              dsm_drone_reinhardshagen_1)
 
-ndsm_aircraft_reinhardshagen2 <- terra::crop(ndsm_aircraft_reinhardshagen,
-                                             dsm_drone_reinhardshagen2)
+ndsm_aircraft_reinhardshagen_2 <- terra::crop(ndsm_aircraft_reinhardshagen_merged,
+                                              dsm_drone_reinhardshagen_2)
 
-ndsm_aircraft_neukirchen91 <- terra::crop(ndsm_aircraft_neukirchen9,
-                                          dsm_drone_neukirchen91)
+ndsm_aircraft_neukirchen8_1 <- terra::crop(ndsm_aircraft_neukirchen8_1_merged,
+                                           dsm_drone_neukirchen8_2)
 
-ndsm_aircraft_neukirchen92 <- terra::crop(ndsm_aircraft_neukirchen9,
-                                          dsm_drone_neukirchen92)
+ndsm_aircraft_neukirchen8_2 <- terra::crop(ndsm_aircraft_neukirchen8_2_merged,
+                                           dsm_drone_neukirchen8_1)
 
+ndsm_aircraft_neukirchen9_1 <- terra::crop(ndsm_aircraft_neukirchen9_merged,
+                                           dsm_drone_neukirchen9_1)
 
-
-
-
-
-
-
+ndsm_aircraft_neukirchen9_2 <- terra::crop(ndsm_aircraft_neukirchen9_merged,
+                                           dsm_drone_neukirchen9_2)
 
 
 
@@ -127,11 +152,19 @@ ndsm_aircraft_neukirchen92 <- terra::crop(ndsm_aircraft_neukirchen9,
 
 
 
-# Plots
+
+
+
+
+
+
+
+
+# Parallel Plots of aircraft and drone nDSMs
 par_org <- par()
 par(mfrow = c(1,2))
-terra::plot(ndsm_aircraft_neukirchen91)
-terra::plot(dsm_drone_neukirchen91)
+terra::plot(ndsm_aircraft_neukirchen9_2)
+terra::plot(dsm_drone_neukirchen9_2)
 par(par_org)
 
 
